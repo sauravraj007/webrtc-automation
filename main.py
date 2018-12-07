733b08161941600cb82df567ace0c3760786bda2
Saurav Raj Srivastava <srajsriv@cisco.com>
2018-12-07 18:12:40 +0530

    fic

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-print "Hello World!"
-print "Hellow Saurav"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
-print "This Chnage is in automation"
+import sys
+with open(sys.argv[1], 'r') as file:
+    for line in file:
+        print line
